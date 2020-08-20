27bf4b63297675bc518516111d636f7388a29d7a
naveen <naveenrevankar53@gmail.com>
2020-08-20 21:51:31 +0530

    committed all

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("hello naveen")
+
+const express = require('express');
+
+const app = express()
+
+app.get ('/help',(req,res)=> {
+    res.send({
+       message :  "help activated"
+    })
+})
+
+app.get ('/',(req,res) => {
+    res.send(" get works ")
+})
+
+app.listen(3000,()=>{
+    console.log("connected to server")
+})
